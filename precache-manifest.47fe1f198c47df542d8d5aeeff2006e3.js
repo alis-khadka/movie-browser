c91913a030ea260ca97539907fa4877e4bcf1157
alis-khadka <alish.khadka@gmail.com>
2020-01-01 23:49:47 +0545

    Updates

---
+++ precache-manifest.47fe1f198c47df542d8d5aeeff2006e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b177c9735b722867779434395d4eb0db",
+    "url": "/movie-browser/index.html"
+  },
+  {
+    "revision": "e33484a1c63f751d911c",
+    "url": "/movie-browser/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e33484a1c63f751d911c",
+    "url": "/movie-browser/static/js/2.badd7d74.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/movie-browser/static/js/2.badd7d74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "161b3d7c45cdd932b49f",
+    "url": "/movie-browser/static/js/main.3b613404.chunk.js"
+  },
+  {
+    "revision": "f99634c0373be14b8a00",
+    "url": "/movie-browser/static/js/runtime-main.265c9c09.js"
+  }
+]);
