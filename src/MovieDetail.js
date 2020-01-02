5075fafc2ee20fafc8f9e3ebf37ad675055d8a8f
alis-khadka <alish.khadka@gmail.com>
2020-01-02 10:17:30 +0545

    Poster on Movie Detail

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -30,39 +30,44 @@ class MovieDetail extends Component {
       Director,
       Writer,
       Actors,
+      Plot,
       Poster,
       imdbRating
     } = this.state.movieData;
 
     return (
-      <Modal
-        {...this.props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            <Col>
-              <Image src={Poster} rounded />
-            </Col>
-            <h2><center>{Title}</center></h2>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <span>
-            <h4>{`Released on: ${Released}`}</h4>
-            <h4>{`IMDB Rating: ${imdbRating}`}</h4>
-          </span>
-          <h5><u>Director:</u> {Director}</h5>
-          <h5><u>Writers:</u> {Writer}</h5>
-          <h5><u>Cast & Crews:</u> {Actors}</h5>
+      <Col md={{ span: 6, offset: 3 }}>
+        <Modal
+          {...this.props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              <Col>
+                <Image src={Poster} rounded />
+              </Col>
+              <h2><center>{Title}</center></h2>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <span>
+              <h4>{`Released on: ${Released}`}</h4>
+              <h4>{`IMDB Rating: ${imdbRating}`}</h4>
+            </span>
+            <h5><u>Director:</u> {Director}</h5>
+            <h5><u>Writers:</u> {Writer}</h5>
+            <h5><u>Cast & Crews:</u> {Actors}</h5>
+            <h5><u>Plot:</u></h5>
+            <p>{Plot}</p>
 
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={this.props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.props.onHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </Col>
     )
   }
 }
